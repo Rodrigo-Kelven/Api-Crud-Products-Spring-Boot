020c979bdcf8f13a19d28ffbecdde44c4d05bc65
Kelven <kelvenr40@gmail.com>
2025-10-26 02:01:06 -0300

    FEAT: update in struct API

---
+++ src/main/java/com/project/base/controllers/ProdutoController.java
@@ -0,0 +1,170 @@
+package com.project.base.controllers;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.project.base.models.Product;
+import com.project.base.services.ProductService;
+
+import com.project.base.dtos.*;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/api/v1/product")
+@Tag(name = "API Product", description = "Endpoints of Products.")
+public class ProdutoController {
+
+
+	// injecao de denpendencias de service
+    private final ProductService service;
+    
+    public ProdutoController(ProductService service) {
+        this.service = service;
+    }
+    
+    
+    
+    // anotacao para dizer que este endpoint é de tipo GET com o path /list
+    // como em java tudo sao classes e funcoes, esta funcao retorna os dados cadastrados, como esta funcao retorna algo, precisa de um tipo de parametro de retorno
+    // logo usa-se a interface List<> com o tipo de parametro Produto -> List<Produto> 
+    // como foi injetado dependencias na variavel repository, o JPARepository realiza tudo de forma automatica, pesquisando, criado, atualizando e deletando
+    @GetMapping("/list")
+    @Operation(
+    		summary = "Retorna todos os produtos", 
+    		description = "Endpoint retorna todos os produtos do Banco de dados."
+    		)
+    @ApiResponses(value = {
+    		@ApiResponse(responseCode = "200", description = "Retornado com Sucesso."),
+    		@ApiResponse(responseCode = "404", description = "Nenhum Produto enconteado.")
+    })
+    public ResponseEntity<List<Product>> listarProdutoController() {
+    	
+    	return service.listarProdutosService();
+    
+    }
+    
+    
+    // anotacao para dizer que este endpoint é de tipo POST com o path /create
+    // de tipo de retorno Produto, a funcao recebe parametros, logo, o Spring disponibiliza uma anotacao @RequestBody, que significa que irá receber parametros do body,
+    // parametros esses de tipo Produto, onde a variavel produto recebe e realiza o save() que de forma automatica o spring com o JPARepository cuida disso
+    @PostMapping("/create")
+    @Operation(
+    		summary = "Criar Produtos", 
+    		description = "Endpoint para a criação de Produtos."
+    		)
+    @ApiResponses(value = {
+    		@ApiResponse(responseCode = "201", description = "Produto criado com Sucesso."),
+    		@ApiResponse(responseCode = "400", description = "Erro ao criar produto.")
+    })
+    public ResponseEntity<ResponseEntity<Product>> criarProdutoController(
+            @Parameter(description = "Dados do Produto", required = true)
+            @Valid @RequestBody ProductRequestDTO productRequestDTO) {
+
+        // Chama o serviço para criar o produto
+        ResponseEntity<Product> product = service.createProdutoService(productRequestDTO);
+
+        // Retorna a resposta com o status 200 (OK) e o Product
+        return ResponseEntity.ok(product);
+    }
+
+
+
+    // anotacao para dizer que este endpoint é de tipo GET com o path /search/{id}
+    // a anotacao PathVariable diz que um parametro virá do path->caminho/url, este parametro é de tipo Lonf com o nome de id
+    // ResponseEntity é uma classe do Spring que representa toda a resposta HTTP, incluindo status, cabeçalhos e corpo.
+    // o repository se reposnsabiliza por buscar o objeto que corresponde ao parametro id que foi passado
+    // .map(ResponseEntity::ok) significa que, se o Optional contém um produto, ele será transformado em um ResponseEntity com status 200 e o produto no corpo. 
+    // ResponseEntity.notFound().build() é uma forma de criar uma resposta HTTP com status 404 Not Found usando a classe ResponseEntity
+    // ResponseEntity.notFound() é um método estático que retorna um construtor (builder) para uma resposta HTTP com status 404.
+    @GetMapping("/search/{id}")
+    @Operation(
+    		summary = "Busca Produto pelo ID", 
+    		description = "Endpoint para busca de Produtos pelo ID"
+    		)
+    @ApiResponses(value = {
+    		@ApiResponse(responseCode = "200", description = "Produto encontrado com Sucesso."),
+    		@ApiResponse(responseCode = "404", description = "Produto não encontrado.")
+    })
+    public ResponseEntity<Product> buscarProdutoByIdController(
+    		@Parameter(description = "ID do Produto a ser procurado", required = true)
+    		@PathVariable UUID id
+    		) {
+    	
+    	return service.searchProdutoById(id);
+    }
+
+    
+    // metodo PUT onde á passagem de parametros tanto pelo caminho/path do endpoint, tando pelo body da requisaicao
+    @PutMapping("/update/{id}")
+    @Operation(
+    		summary = "Atualiza Produto pelo ID",
+    		description = "Endpoint para realizar atualização do Produto pelo ID"
+    		)
+    @ApiResponses(value = {
+    		@ApiResponse(responseCode = "200", description = "Produto atualizado com Sucesso."),
+    		@ApiResponse(responseCode = "404", description = "Produto nao encontrado")
+    })
+    public ResponseEntity<Product> atualizarProdutoByIdController(
+    		@Parameter(description = "ID do Produto a ser atualizado.", required = true) 
+    		@PathVariable(required = true) UUID id,
+    		@RequestBody ProductRequestDTO produtoAtualizado ) {
+    	
+    	return service.updateProdutoService(id, produtoAtualizado);
+        
+    	}
+    
+    // endpoint Delete que recebe parametros pelo caminho/path do endpoint
+    // ResponseEntity por ser uma resposta HTTP,status e corpo, retorna void, somente o status code ser retornado
+    @DeleteMapping("/delete/{id}")
+    @Operation(
+    		summary = "Deletar Produto pelo ID",
+    		description = "Enpoint para deletar produto pelo ID"
+    		)
+    @ApiResponses(value = {
+    		@ApiResponse(responseCode = "204", description = "Produto deletado com Sucesso."),
+    		@ApiResponse(responseCode = "404", description = "Produto não encontrado.")
+    })
+    public ResponseEntity<Void> deletarProdutoByIdController(
+    		@Parameter(description = "ID do produto a ser deletado", required = true)
+    		@PathVariable UUID id ) {
+    	
+    	return service.deletarProdutoById(id);
+    	
+    }
+
+    
+    @DeleteMapping("/delete")
+    @Operation(
+    		summary = "Deletar todos os Produtos",
+    		description = "Endpoint para deletar todos os Produtos"
+    		)
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "204", description = "Produtos apagados com Sucesso."),
+        @ApiResponse(responseCode = "404", description = "Nenhum Produto encontrado.")
+    })
+    public ResponseEntity<Void> deletarProdutosAllController() {
+       
+    	return service.deleteProdutoAll();
+    	
+    }
+
+    
+    
+    
+}
---
+++ src/main/java/com/project/base/repository/ProdutoRepository.java
@@ -0,0 +1,16 @@
+package com.project.base.repository;
+
+import com.project.base.models.Product;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+// aqui basicamente a interface ProdutoRepository esta fazendo um extends de JpaRepository para receber metodos para realizar o crud
+// A interface ProdutoRepository está estendendo JpaRepository para herdar métodos prontos de CRUD (como save, findById, findAll, deleteById, etc).
+// JpaRepository é uma interface genérica do Spring Data JPA, por isso exige dois parâmetros:
+// 1. Produto – a entidade (classe anotada com @Entity) que representa a tabela no banco de dados.
+// 2. Long – o tipo do identificador (ID) da entidade Produto, ou seja, o tipo da chave primária.
+public interface ProdutoRepository extends JpaRepository<Product, UUID> {
+}
